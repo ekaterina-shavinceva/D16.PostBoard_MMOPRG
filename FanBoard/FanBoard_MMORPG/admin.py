c656272cb655fe7af5942894457c6b089d35607f
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-11-04 13:11:54 +0500

    Delete FanBoard directory

--- FanBoard/FanBoard_MMORPG/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Article, UserResponse, Subscription
-
-
-admin.site.register(Article)
-admin.site.register(UserResponse)
-admin.site.register(Subscription)
