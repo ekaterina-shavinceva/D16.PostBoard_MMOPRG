fbec6fab8ce28efe4a8fe0940dd24dcf731f148a
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-11-05 16:32:19 +0500

    Delete FanBoard directory

--- FanBoard/FanBoard_MMORPG/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ArticleList, ArticleDetail
-
-
-urlpatterns = [
-    path('', ArticleList.as_view()),
-    path('<int:pk>', ArticleDetail.as_view()),
-]
