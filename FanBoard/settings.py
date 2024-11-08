2879538cec27fef708a12811a0cce1a2c1495f75
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-11-08 14:21:23 +0500

    Update settings.py

--- FanBoard/settings.py
+++ FanBoard/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'FanBoard.urls'
