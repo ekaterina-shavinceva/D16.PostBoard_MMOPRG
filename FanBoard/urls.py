d889af3f6b2cf21a380035a38fda8b7b37cc4c65
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-11-09 15:28:27 +0500

    Update urls.py

--- FanBoard/urls.py
+++ FanBoard/urls.py
@@ -24,12 +24,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('django.contrib.flatpages.urls')),
-    # path('sign/', include('django.contrib.auth.urls')),
-    # path("sign/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),
     path('articles/', include('FanBoard_MMORPG.urls')),
-    # path('logout/', LogoutView.as_view()),
- ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ ] 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
