661c2b4d33807c9ae4540eaa7e2a42766be7ebb8
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-11-11 14:13:07 +0500

    Delete FanBoard_MMORPG directory

--- FanBoard_MMORPG/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from .models import Article
-
-
-@receiver(post_save, sender=Article)
-def article_created(instance, **kwargs):
-    print('Новое объявление', instance)
